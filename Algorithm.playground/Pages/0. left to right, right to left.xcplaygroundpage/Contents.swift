b3ead9b747beee9709767f6468e477ec1342c674
ismayilovmurad <ismayilovmurad@outlook.com>
2022-05-11 18:12:19 +0400

    lexicographically smaller, 0. left to right, right to left, 1. (i-1) + i + (i+1)

---
+++ Algorithm.playground/Pages/0. left to right, right to left.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var a = [1, 2, 3]
+var b = [1, 2, 3]
+var k = 31
+
+func solution(a: [Int], b: [Int], k: Int) -> Int {
+    var tinyCounter = 0
+    
+    // iterate through from 0 to the length of the a or b, they are the same length
+    for i in 0..<a.count {
+        // x = i-th item of the a, from the left
+        let x = String(a[i])
+        // y = i-th item of the b, from the right
+        let y = String(b[b.count-(i+1)])
+        // concatenate xy
+        let xy = Int(x + y)!
+        // check if it's less than k
+        if xy < k {
+            tinyCounter += 1
+        }
+    }
+    
+    return tinyCounter
+}
+
+print(solution(a: a, b: b, k: k))
